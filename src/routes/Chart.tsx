3bcd435657feae4ee73201d19d8cf6851913c04d
JaYun <tlfvm04@naver.com>
2022-11-12 01:32:32 +0900

    chart

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,5 +1,58 @@
-function Chart() {
-    return <h1>Chart</h1>;
-  }
+import { useQuery } from "react-query";
+import { fetchCoinHistory } from "../api";
+import ApexChart from "react-apexcharts";
+
+interface ChartProps {
+  coinId: string;
+}
+
+interface IHistorical {
+  time_open: number;
+  time_close: number;
+  open: string;
+  high: string;
+  low: string;
+  close: string;
+  volume: string;
+  market_cap: number;
+}
+
+function Chart({ coinId }: ChartProps) {
+  const { isLoading, data } = useQuery<IHistorical[]>(["ohlcv", coinId], () =>
+    fetchCoinHistory(coinId)
+  );
+  return (
+    <div>
+      {isLoading ? ( "Loading chart..." ) : (
+        <ApexChart
+          type="line"
+          series={[
+            {
+              name: "Price",
+              data: data?.map((price) => Number(price.close)) as number[],
+            },
+          ]}
+          options={{
+            theme: {
+              mode: "dark",
+            },
+            chart: {
+              height: 300,
+              width: 500,
+              toolbar: {
+                show: false,
+              },
+              background: "transparent",
+            },
+            stroke: {
+              curve: "smooth",
+              width: 4,
+            },
+          }}
+        />
+      )}
+    </div>
+  );
+}
   
   export default Chart;
